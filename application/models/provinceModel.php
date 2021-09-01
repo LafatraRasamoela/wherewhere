6fa5f8f2d3d5a1563d4acfc8eec0f4f71878265a
LalaPaul <81089666+LalaPaul@users.noreply.github.com>
2021-09-01 10:25:12 -1000

    quelque modificateion au niveau du provinceModel.php

--- application/models/provinceModel.php
+++ application/models/provinceModel.php
@@ -5,47 +5,5 @@
             $req = $this->db->get('province');
             return $req->result_array();
         }
-
-        // function getModel(){
-        //     $query = $this->db->query('SELECT * FROM province');
-        //     $tab = array();
-        //     foreach($query->result_array() as $row){
-        //         $prov = new provinceModel();
-        //         $prov -> setIdProvince($row['idProvince']);
-        //         $prov -> setNomProvince($row['nomProvince']);
-        //         $prov -> setImageProvince($row['imageProvince']);
-        //         $tab[] = $prov; 
-        //     }
-        //     return $tab;
-        // }
-
-
-        // public function getIdProvince()
-        // {
-        //     return $this->idProvince;
-        // }
-        // public function setIdProvince($id)
-        // {
-        //     $this->idProvince = $id;
-        //     return $this;
-        // }
-        // public function getNomProvince()
-        // {
-        //     return $this->nomProvince;
-        // }
-        // public function setNomProvince($nom)
-        // {
-        //     $this->nomProvince = $nom;
-        //     return $this;
-        // }
-        // public function getImageProvince()
-        // {
-        //     return $this->imageProvince;
-        // }
-        // public function setImageProvince($image)
-        // {
-        //     $this->imageProvince = $image;
-        //     return $this;
-        // }
     }
 ?>
